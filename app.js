c4e8676a38a13bb018318245ac3542deac738905
Peter Michaleczky <peter.michaleczky@itron.com>
2019-11-05 13:14:23 +0100

    add members list command

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
 const yargs = require('yargs');
-const helpers = require('./lib/helpers');
+const commands = require('./lib/Commands');
 
-const argv = yargs
-    .usage('$0 <command> [options]')
-    .command('members', 'List members')
-    .demandOption('n')
-    .example('$0 members [--store|-s]')
-    .alias('n', 'name')
-    .describe('n', 'Provide YahooGroups\' group name')
-    .nargs('n', 1)
-    .alias('s', 'store')
-    .describe('s', 'Store results in the local storage')
-    .nargs('s', 1)
-    .help('h')
-    .alias('h', 'help')
-    .argv;
-
-let secrets;
-
-secrets = helpers.readJSONAsync('./secrets.json').then((secrets) => {
-    console.dir(argv);
-    if (argv.members) {
-        const api = new YahooGroupsAPI('test', secrets.YahooGroups);
-    }
-});
+yargs.usage('$0 <command> [options]');
+for (let commandClass of [commands.MembersListCommand]) {
+    let command = new commandClass();
+    yargs.command(command.command, command.description, command.buildOptions, command.action);
+}
+yargs.help('h').alias('h', 'help');
+yargs.argv;
