27818e47b41881be5bae11d38a1f2bfae8461d08
Peter Michaleczky <peter.michaleczky@itron.com>
2019-11-30 09:45:25 +0100

    add Storage class and refactor entities

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const yargs = require('yargs');
 const commands = require('./lib/Commands');
 
+const commandList = [
+    commands.MembersListCommand,
+    commands.MessageListCommand,
+    commands.ArchiveMessagesCommand
+];
+
 yargs.usage('$0 <command> [options]');
-for (let commandClass of [commands.MembersListCommand, commands.MessageListCommand]) {
+for (let commandClass of commandList) {
     let command = new commandClass();
     yargs.command(command.command, command.description, command.buildOptions, command.action);
 }
